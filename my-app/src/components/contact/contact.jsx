5b49bd23ac6c95df1d7772aa879e1b4f2a68c3e4
Mikhail Karyagin <notan02091990@gmail.com>
2023-03-09 21:43:34 +0500

    Made an adaptation to 1024px

--- my-app/src/components/contact/contact.jsx
+++ my-app/src/components/contact/contact.jsx
@@ -13,7 +13,7 @@ function Contact(){
                 <p className="contactTitle">Наши контакты:</p>
                 
                 <ul className="contactList">
-                    <li className="contactItem"> Телефон: <a href="+79193965351" lassName="contactLink" target="_blank">8(919)396-53-51</a>,(с 8.00 до 20.00 по моск. времени)</li>
+                    <li className="contactItem"> Телефон: <a href="+79193965351" lassName="contactLink" target="_blank">8 (919) 396-53-51</a>,(с 8.00 до 20.00 по моск. времени)</li>
                     <li className="contactItem">E-mail: <a href="notan02@mail.ru" lassName="contactLink" target="_blank">Notan02@mail.ru</a></li>
                     <li className="contactItem">Наш адрес: г. Нижний Тагил, Свердловская область</li>
                 </ul>
--- my-app/src/components/footer/footer.jsx
+++ my-app/src/components/footer/footer.jsx
@@ -10,18 +10,18 @@ function Footer (){
   <div className="container">
     <div className="footerColumn">
       <nav className="footerNav">
-      <Link to="/about" className = ""> <a href="#">О нас</a></Link>
-      <Link to="/contact" className = ""><a href="#">Наши контакты</a></Link>
-      <Link to="/form" className = ""><a href="#">Заказать</a></Link>
+        <Link to="/about"> <a href="/#" className="footerNavLink" rel="noreferrer">О нас</a></Link>
+        <Link to="/contact"><a href="/#"className="footerNavLink" rel="noreferrer">Наши контакты</a></Link>
+        <Link to="/form"><a href="/#" className="footerNavLink" rel="noreferrer">Заказать</a></Link>
       </nav>
 
 
       <div className="footerSocial">
-        <a href="https://vk.com/vogulgems" className="footerLink" target="_blank">
+        <a href="https://vk.com/vogulgems" className="footerLink" target="_blank" rel="noreferrer">
           <img src= "./img/social/contact1.png" alt="Мы в контакте" className="footerImg" />
         </a>
       
-        <a href="https://www.livemaster.ru/artelvogul" className="footerLink" target="_blank">
+        <a href="https://www.livemaster.ru/artelvogul" className="footerLink" target="_blank" rel="noreferrer">
           <img src="./img/social/contact3.png" alt="Мы на ярмарке мастеров" className="footerImg" />
         </a>
       </div>
